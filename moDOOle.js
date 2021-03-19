01af2029e0d6da5fee678ade19b510b9eeacfb14
Petr Prudil <petrprudil2005@seznam.cz>
2021-03-19 17:07:49 +0100

    change of file names

---
+++ moDOOle.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+//var fromIndex = require('./app.js');
+var md = require('./moDOOle.js');
+
+exports.route = function (routeName, fileName) {
+    req = fromIndex.getReq();
+    res = fromIndex.getRes();
+
+        if(req.url == routeName){
+            md.showHTML(fileName, res);
+        }
+};
+
+exports.showHTML = function (fileName, res) {
+            fs.readFile("./routes/" + fileName, function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+};
