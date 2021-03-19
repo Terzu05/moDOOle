38908a9a0d4aa4f1d21bcdc3cf49f8e9c8a1fe86
Petr Prudil <petrprudil2005@seznam.cz>
2021-03-19 18:38:19 +0100

    semi-functional

--- moDOOle.js
+++ moDOOle.js
@@ -1,26 +1,54 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-//var fromIndex = require('./app.js');
-var md = require('./moDOOle.js');
+const doo = require('modoole');
+
+var viewFolder
+var reqVar;
+var resVar;
+
+exports.setViewFolder = function (folderName = './views') {
+    viewFolder = folderName;
+};
 
 exports.log = function () {
-    console.log('looool')
+    req = reqVar;
+    res = resVar;
+    var q = url.parse(req.url, true);
+    console.log('lolol = ' + q.pathname)
 };
 
-exports.route = function (routeName, fileName) {
-    req = fromIndex.getReq();
-    res = fromIndex.getRes();
-
-        if(req.url == routeName){
-            md.showHTML(fileName, res);
-        }
+exports.functTest = function (port = 5000, code = function code(){}){
+    code();
 };
 
-exports.showHTML = function (fileName, res) {
-            fs.readFile("./routes/" + fileName, function(err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-            });
+exports.web = function (port = 5000, code = function code(){}){
+    http.createServer(function (req, res) {
+        reqVar = req;
+        resVar = res;
+        code();
+      }).listen(port);
+};
+
+exports.route = function (fileName, routeName = ''){
+    req = reqVar;
+    res = resVar;
+    var q = url.parse(req.url, true);
+
+    routeName = '/' + routeName;
+
+    if(routeName == q.pathname){
+        doo.renderHTML(fileName)
+    }
+    
+};
+
+exports.renderHTML = function (fileName){
+    req = reqVar;
+    res = resVar;
+    fs.readFile(fileName, function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 };
